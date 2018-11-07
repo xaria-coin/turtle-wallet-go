739961d76dbbe81e7b92128ab4aca7e4264e8a7a
andrew kelly <andyckelly@outlook.com>
2018-11-07 19:22:43 +0000

    minor update

--- constants.go
+++ constants.go
@@ -1,12 +1,12 @@
 package main
 
 const (
-	versionGUI                 = "0.02"
+	versionGUI                 = "0.04"
 	urlCryptoCompareXARI        = "http://xaria.xyz/price.json"
 	defaultRemoteDaemonAddress  = "node.xaria.xyz"
 	defaultRemoteDaemonPort     = "11966"
 	logFileFilename             = "xaria-GUI-logs.log"
-	urlBlockExplorer            = "http:/explorer.xaria.xyz"
+	urlBlockExplorer            = "http://explorer.xaria.xyz"
 	dbFilename                  = "settings.db"
 	numberTransactionsToDisplay = 20
 )
